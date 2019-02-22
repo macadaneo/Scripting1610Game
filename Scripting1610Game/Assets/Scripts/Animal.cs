bf221b5654b0e35c0401f454c2d8b387bad21884
Dane C. Ericksen <10721110@my.uvu.edu>
2019-02-21 20:06:16 -0700

    Large chunk of work done on Assignment 0 & 1

---
+++ Scripting1610Game/Assets/Scripts/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    public string Name;
+    public Color SkinColor;
+    public Color EyeColor;
+    public floatData health;
+    public floatData speed;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripting1610Game/Assets/Scripts/SpaceShipEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpaceShipEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+    private void OnCollisionEnter2D()
+    {
+        Event.Invoke();
+    }
+}
