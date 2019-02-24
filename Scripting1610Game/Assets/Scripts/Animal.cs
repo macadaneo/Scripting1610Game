26180bd8fa296713e83e347259aa0a3b8cbd0fbe
Dane C. Ericksen <10721110@my.uvu.edu>
2019-02-24 02:02:00 -0700

    Added Float Data and Game Variables

--- Scripting1610Game/Assets/Scripts/Animal.cs
+++ Scripting1610Game/Assets/Scripts/Animal.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Animal : MonoBehaviour
 {
     public string Name;
+    public int FurCount = 10000;
+    public float EatingSpeed = 0.4f;
+    public bool CanEat = true;
+    public UnityEvent Event;
     public Color SkinColor;
     public Color EyeColor;
-    public floatData health;
-    public floatData speed;
+    public floatData Health;
+    public floatData Speed;
+    public bool CanMove;
 
     private void Start()
     {
-        
+        Event.Invoke();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       //Do Work 
     }
 }
--- Scripting1610Game/Assets/Scripts/Deer.cs
+++ Scripting1610Game/Assets/Scripts/Deer.cs
@@ -8,12 +8,13 @@ public class Deer : Mamal
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<SpriteRenderer>().color = PrimaryHairColor;
+        Event.Invoke();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0,1,0);
     }
 }
