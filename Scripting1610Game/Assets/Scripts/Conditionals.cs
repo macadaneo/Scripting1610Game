95ef8f78992ec9b6f5695f9a59e3afef1ca3e156
Dane Ericksen <10721110@my.uvu.edu>
2019-02-25 11:15:30 -0700

    Windmill Anim, If and else statements, human model

---
+++ Scripting1610Game/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    public int Number = 11;
+    public string Password;
+    public bool CanWalk;
+    
+    
+    void Update()
+    {
+        if (CanWalk)
+        {
+            print("true");
+        }
+        else
+        {
+            print(false);
+        }
+
+        if (Number >= 10)
+        {
+            print("greater");
+        }
+        else
+        {
+            print("lesser");
+        }
+        
+        if (Password == "DOOM2016")
+        {
+            print("Correct");
+        }
+        else
+        {
+            print("Incorrect");
+        }
+    }
+}
