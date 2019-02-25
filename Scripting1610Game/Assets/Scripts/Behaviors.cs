91decbc7eebbd9a6495a58358d28943c541efca7
Dane C. Ericksen <10721110@my.uvu.edu>
2019-02-24 23:04:21 -0700

    Added WIndmill and Animation
    also added more variables

---
+++ Scripting1610Game/Assets/Scripts/Behaviors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Behaviors : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEvent Event;
+
+    private void Start()
+    {
+        Event.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Event.Invoke();
+    }
+
+    void OnCollisionEnter()
+    {
+        Event.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Event.Invoke();
+    }
+}
---
+++ Scripting1610Game/Assets/Scripts/updating.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updating : MonoBehaviour
+{
+    public bool _canRun = true;
+    
+    private WaitForFixedUpdate _fixed;
+
+    public bool CanRun
+    {
+        get { return _canRun; }
+        set { _canRun = value; }
+    }
+
+    public void BeginCoroutine()
+    {
+        CanRun = true;
+        StartCoroutine(RunCoroutine());
+    }
+    
+    
+    
+    private IEnumerator RunCoroutine()
+    {
+        _fixed = new WaitForFixedUpdate();
+        yield return _fixed;
+        print("Start");
+        while (CanRun)
+        {
+           yield return _fixed;
+           print("Running");
+        }
+    }
+}
