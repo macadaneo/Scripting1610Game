ed48b426ce467e0b4343739d1f3453d3924ae4f3
Dane C. Ericksen <10721110@my.uvu.edu>
2019-03-01 22:59:32 -0700

    Created "light Switch" campfire

---
+++ Scripting1610Game/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LightSwitch : MonoBehaviour
+{
+    public UnityEvent Event;
+
+    public bool DinnerTime;
+
+    private void Update()
+    {
+        if (DinnerTime)
+        {
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+            Event.Invoke();
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.black;
+            Event.Invoke();
+        }
+    }
+
+}  
