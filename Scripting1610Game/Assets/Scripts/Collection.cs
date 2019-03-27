2d837e244cffbbf205cfbfc10fe913aa2cb8e302
Dane Ericksen <10721110@my.uvu.edu>
2019-03-27 11:16:25 -0600

    Added to collection scripts

--- Scripting1610Game/Assets/Scripts/Collection.cs
+++ Scripting1610Game/Assets/Scripts/Collection.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Collection : ScriptableObject
 {
-    public List<Object> CollectionList;
+    public List<floatData> CollectionList;
     
     
-    public void Collect(Object obj)
+    public void Collect(floatData obj)
     {
             CollectionList.Add(obj);
     }
-    
+
+    public void CollectionInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            Debug.Log(obj);
+        }
+    }
+
+    public void AmmoInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            if (obj.name == "Ammo")
+            {
+                Debug.Log("We have " + obj.Value + " ammo.");
+            }
+        }
+    }
+
+    public void FuelInfo()
+    {
+        for (int i = 0; i < CollectionList.Count; i++)
+        {
+            if (CollectionList[i].name == "Fuel")
+            {
+                Debug.Log(CollectionList[i]);
+            }
+        }
+    }
 }
