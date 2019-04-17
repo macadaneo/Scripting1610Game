de0431383132d286f7f8a8204cef4ab65e944da6
Dane Ericksen <10721110@my.uvu.edu>
2019-04-17 10:46:01 -0600

    Added Running Animation Script

---
+++ Scripting1610Game/Assets/Scripts/RunAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class RunAnim : MonoBehaviour
+{
+    private Animator anims;
+    public float Speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+       anims.SetFloat("Speed",Speed); 
+    }
+}
