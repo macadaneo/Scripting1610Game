d728bd5500e358f8ebf1ebf957749a8808c6a465
Dane C. Ericksen <10721110@my.uvu.edu>
2019-04-19 00:00:59 -0600

    Added Counter Script

---
+++ Scripting1610Game/Assets/Scripts/Counter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+
+    public int Value = 3, MinValue = 0;
+    public float WaitTime = 1;
+
+    private IEnumerator Start()
+    {
+        var waitObj = new WaitForSeconds(WaitTime);
+        
+        while (Value > MinValue)
+        {
+            yield return waitObj;
+            print(Value);
+            Value--;
+        }
+    }
+}
