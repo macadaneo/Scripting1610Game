b20511a82e281ac44f4daa66a8cf57e650f1d60e
Dane C. Ericksen <10721110@my.uvu.edu>
2019-04-22 23:52:11 -0600

    main character now moves
    need to make sure he cant fly when i hit the jump key. also need to implement rotation animation.

---
+++ Scripting1610Game/Assets/Scripts/PoeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoeController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 postition = transform.position;
+        postition.x = postition.x + 0.1f;
+        transform.position = postition;
+    }
+}
