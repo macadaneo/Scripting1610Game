890cd06c51d49ece97d1d2cd6e9eefdb0a3993c3
Dane C. Ericksen <10721110@my.uvu.edu>
2019-04-25 21:08:14 -0600

    Added health collectible
    added collection script and what not

---
+++ Scripting1610Game/Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        PoeController controller = other.GetComponent<PoeController>();
+
+        if (controller!= null)
+        {
+            if (controller.currentHealth < controller.maxHealth)
+            {
+                controller.currentHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Scripting1610Game/Assets/Scripts/PoeController.cs
+++ Scripting1610Game/Assets/Scripts/PoeController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PoeController : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public float speed = 3.0f;
+    
+     public int maxHealth = 5;
 
-    void Update()
-    {
-        Vector2 postition = transform.position;
-        postition.x = postition.x + 0.1f;
-        transform.position = postition;
-    }
+     public int health{get {return currentHealth;}}
+     private int currentHealth;
+
+     public Rigidbody rigidbody;
+
+        public void Start()
+        {
+          rigidbody = GetComponent<Rigidbody>();
+  
+          currentHealth = maxHealth;
+        }
+
+
+        public void Update()
+        {
+          float horizontal = Input.GetAxis("Horizontal");
+          float vertical = Input.GetAxis("Vertical");
+          
+          Vector2 position = rigidbody.position;
+          position.x = position.x + 3.0f* horizontal * Time.deltaTime;
+          position.y = position.y + 3.0f * vertical * Time.deltaTime;
+          
+          
+          rigidbody.MovePosition(position);
+        }
+
+        public void ChangeHealth(int amount)
+        {
+          currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+          Debug.Log(currentHealth + "/" + maxHealth);
+        }
 }
